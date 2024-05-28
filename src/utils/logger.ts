8f388ff5a8cdd65f22be9afa6e9b50f02f2dd7d4
JT Linco <joel.t.cofie@gmail.com>
2024-05-28 14:23:29 +0000

    code cleanup; lint and formatting update (#1)

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,19 +1,19 @@
-import chalk from "chalk";
+import chalk from 'chalk';
 
 export const logger = {
-  error(...args: unknown[]) {
-    console.log(chalk.redBright(...args));
-  },
-  warn(...args: unknown[]) {
-    console.log(chalk.yellowBright(...args));
-  },
-  info(...args: unknown[]) {
-    console.log(chalk.cyanBright(...args));
-  },
-  success(...args: unknown[]) {
-    console.log(chalk.greenBright(...args));
-  },
-  break() {
-    console.log("");
-  },
+	error(...args: unknown[]) {
+		console.log(chalk.redBright(...args));
+	},
+	warn(...args: unknown[]) {
+		console.log(chalk.yellowBright(...args));
+	},
+	info(...args: unknown[]) {
+		console.log(chalk.cyanBright(...args));
+	},
+	success(...args: unknown[]) {
+		console.log(chalk.greenBright(...args));
+	},
+	break() {
+		console.log('');
+	},
 };
--- src/utils/scaffoldProject.ts
+++ src/utils/scaffoldProject.ts
@@ -1,54 +1,55 @@
-import chalk from "chalk";
-import path from "path";
+import chalk from 'chalk';
+import path from 'node:path';
 // import { PKG_ROOT } from "~/consts.js";
 // import * as p from "@clack/prompts";
-import { logger } from "./logger.js";
+import { logger } from './logger.js';
 // import ora from "ora";
 // import fs from "fs-extra";
-import { promptProject } from "./prompts.js";
-import { webAppInstaller } from "~/helpers/webAppInstaller.js";
-import { mobileAppInstaller } from "~/helpers/mobileAppInstaller.js";
+import { promptProject } from './prompts.js';
+import { webAppInstaller } from '~/helpers/webAppInstaller.js';
+import { mobileAppInstaller } from '~/helpers/mobileAppInstaller.js';
 
-export type ProjectType = "webApp" | "mobileApp" | "crossPlatformApp";
+export type ProjectType = 'webApp' | 'mobileApp' | 'crossPlatformApp';
 export async function createProject({
-  projectType,
-  workingFolder,
+	projectType,
+	workingFolder,
 }: {
-  projectType: ProjectType | unknown;
-  workingFolder: string;
+	projectType: ProjectType | unknown;
+	workingFolder: string;
 }) {
-  const { projectName, appDir } = await promptProject();
-  const projectDir = path.join(workingFolder, appDir);
+	const { projectName, appDir } = await promptProject();
+	const projectDir = path.join(workingFolder, appDir);
 
-  projectType === 'webApp' && await webAppInstaller({
-    appDir,
-    projectDir,
-    projectName,
-    projectType,
-    workingFolder
-  })
+	projectType === 'webApp' &&
+		(await webAppInstaller({
+			appDir,
+			projectDir,
+			projectName,
+			projectType,
+			workingFolder,
+		}));
 
-  projectType === 'mobileApp' && await mobileAppInstaller({
-    appDir,
-    projectDir,
-    projectName,
-    workingFolder
-  })
+	projectType === 'mobileApp' &&
+		(await mobileAppInstaller({
+			appDir,
+			projectDir,
+			projectName,
+			workingFolder,
+		}));
 
+	if (projectType === 'crossPlatformApp') {
+		logger.info(
+			`Cross-Platform App: ${chalk.yellow(
+				"We'll be bringing the setup for this soon",
+			)}`,
+		);
+		logger.info('Exiting...');
+		process.exit(1);
+	}
 
-  if (projectType === "crossPlatformApp") {
-    logger.info(
-      `Cross-Platform App: ${chalk.yellow(
-        "We'll be bringing the setup for this soon"
-      )}`
-    );
-    logger.info(`Exiting...`);
-    process.exit(1);
-  }
-
-  return {
-    projectDir,
-    projectName,
-    projectType,
-  };
+	return {
+		projectDir,
+		projectName,
+		projectType,
+	};
 }
